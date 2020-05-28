e509381e68d8cb9c0db0e7115a81a460ffeee6aa
Sean Bocirnea <sean@aetheris.ca>
2020-05-28 00:03:05 -1000

    Rev. 4

--- keyboards/kbdfans/kbd75/keymaps/cata/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/cata/keymap.c
@@ -15,11 +15,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   //QWERTY Base
   [_QW] = LAYOUT_ansi(
-    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_BRK,   KC_INS,   KC_DEL,
-    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,             KC_BSPC,  KC_HOME,
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_END,
-    XXXXXXX,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_PGUP,
-    KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_PGDN,
+    KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_BRK,   KC_PGUP,  KC_PGDN,
+    KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,             KC_BSPC,  KC_INS,
+    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_DEL,
+    XXXXXXX,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                      KC_ENT,   KC_HOME,
+    KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,            KC_UP,    KC_END,
     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT,            MO(_FN),  KC_LEFT,  KC_DOWN,  KC_RGHT
   ),
 
@@ -63,9 +63,9 @@ const rgblight_segment_t* const PROGMEM rgb_layers[] = RGBLIGHT_LAYERS_LIST(
     rgb_layer_capslock,
     rgb_layer_scrllock,
     rgb_layer_numlock,
-    rgb_layer_FA,
     rgb_layer_aff,
-    rgb_layer_neg
+    rgb_layer_neg,
+    rgb_layer_FA
 );
 
 
@@ -76,7 +76,7 @@ void keyboard_post_init_user(void) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    rgblight_set_layer_state(3, layer_state_cmp(state, 2));
+    rgblight_set_layer_state(5, layer_state_cmp(state, 2));
     return state;
 }
 
@@ -90,15 +90,15 @@ bool led_update_user(led_t led_state) {
 void post_process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case C(S(KC_B)):
-      rgblight_blink_layer(4, 3000);
+      rgblight_blink_layer(3, 3000);
       break;
     case C(KC_B):
-      rgblight_blink_layer(5, 3000);
+      rgblight_blink_layer(4, 3000);
       break;
     case NK_TOGG:
     case NK_ON:
     case NK_OFF:
-        rgblight_blink_layer(keymap_config.nkro ? 4 : 5, 500);
+        rgblight_blink_layer(keymap_config.nkro ? 3 : 4, 500);
         break;
   }
 }
